18e1f41fc6061ed32baf0866c7b31f81d606b5e0
Italo Mandara <italomandara@gmail.com>
2026-02-12 23:38:27 +0100

    layout and functionality improvements

--- Procyon/Components/GameOptionsView.swift
+++ Procyon/Components/GameOptionsView.swift
@@ -17,81 +17,88 @@ struct GameOptionsView: View {
     
     var body: some View {
         let gameOptKey = namespacedKey("GameOptions", String(game!.id))
-        
-        Form {
-            VStack(alignment: .leading, spacing: 20) {
-                Section("Launch options") {
-                    HStack(alignment: .top, spacing: 20) {
-                        VStack(alignment: .leading, spacing: 20){
-                            Picker("Graphics Backend", selection: $gameOptions.cxGraphicsBackend) {
-                                Text("D3DMetal")
-                                    .tag("d3dmetal")
-                                Text("DXMT")
-                                    .tag("dxmt")
+        VStack (alignment: .leading, spacing: 5){
+            Text("Options for \(game!.name) id:\(game!.id)").font(Font.footnote).foregroundStyle(.procyonBrightGray)
+            Form {
+                VStack(alignment: .leading, spacing: 20) {
+                    Section("Generic options") {
+                        HStack(alignment: .top, spacing: 20) {
+                            VStack(alignment: .leading, spacing: 20){
+                                Picker("Graphics Backend", selection: $gameOptions.cxGraphicsBackend) {
+                                    Text("D3DMetal")
+                                        .tag("d3dmetal")
+                                    Text("DXMT")
+                                        .tag("dxmt")
+                                }
+                                .pickerStyle(.menu)
+                                TextField("Game arguments", text: $gameOptions.gameArguments)
+                                TextField("Env variables", text: $gameOptions.envVariables)
                             }
-                            .pickerStyle(.menu)
-                            TextField("Game arguments", text: $gameOptions.gameArguments)
-                            TextField("Env variables", text: $gameOptions.envVariables)
-                        }
-                        Spacer()
-                        VStack(alignment: .trailing) {
-                            Toggle("MSync", isOn: $gameOptions.wineMSync)
-                            Toggle("Metal HUD", isOn: $gameOptions.mtlHudEnabled)
-                            Toggle("Advertise AVX", isOn: $gameOptions.advertiseAVX)
+                            Spacer()
+                            VStack(alignment: .trailing) {
+                                Toggle("MSync", isOn: $gameOptions.wineMSync)
+                                Toggle("Metal HUD", isOn: $gameOptions.mtlHudEnabled)
+                                Toggle("Advertise AVX", isOn: $gameOptions.advertiseAVX)
+                            }
                         }
                     }
-                }
-                
-                if(gameOptions.cxGraphicsBackend == "dxmt") {
-                    Divider()
-                    Section("DXMT Options") {
-                        VStack{
-                            Text(localizedString(forKey: "preferredMaxFrameRate", value: preferredMaxFrameRate))
-                            Slider(
-                                value: $gameOptions.dxmtPreferredMaxFrameRate,
-                                in: 19...240,
-                                step: 1.0
-                            )
-                            .help(localizedString(forKey: "preferredMaxFrameRateHelp"))
-                        }
-                        
-                        Toggle("metalFXSpatial", isOn: $gameOptions.dxmtMetalFXSpatial)
-                            .help(localizedString(forKey: "metalFXSpatialHelp"))
-                            .onChange(of: gameOptions.dxmtMetalFXSpatial) { oldValue, newValue in
-                                if (!newValue) {
-                                    $gameOptions.dxmtMetalSpatialUpscaleFactor.wrappedValue = 1.0
+                    if(gameOptions.cxGraphicsBackend == "dxmt") {
+                        Divider()
+                        Section("DXMT Options") {
+                            VStack{
+                                Text(localizedString(forKey: "preferredMaxFrameRate", value: preferredMaxFrameRate))
+                                Slider(
+                                    value: $gameOptions.dxmtPreferredMaxFrameRate,
+                                    in: 19...240,
+                                    step: 1.0
+                                )
+                                .help(localizedString(forKey: "preferredMaxFrameRateHelp"))
+                            }
+                            
+                            Toggle("metalFXSpatial", isOn: $gameOptions.dxmtMetalFXSpatial)
+                                .help(localizedString(forKey: "metalFXSpatialHelp"))
+                                .onChange(of: gameOptions.dxmtMetalFXSpatial) { oldValue, newValue in
+                                    if (!newValue) {
+                                        $gameOptions.dxmtMetalSpatialUpscaleFactor.wrappedValue = 1.0
+                                    }
+                                }
+                            
+                            if (gameOptions.dxmtMetalFXSpatial) {
+                                VStack {
+                                    Text(localizedString(forKey:"metalSpatialUpscaleFactor", value: String($gameOptions.dxmtMetalSpatialUpscaleFactor.wrappedValue)))
+                                    Slider(
+                                        value: $gameOptions.dxmtMetalSpatialUpscaleFactor,
+                                        in: 1.0...2.0,
+                                        step: 0.125
+                                    )
+                                    .help(localizedString(forKey: "metalFXSpatialHelp"))
                                 }
                             }
-                        
-                        if (gameOptions.dxmtMetalFXSpatial) {
-                            VStack {
-                                Text(localizedString(forKey:"metalSpatialUpscaleFactor", value: String($gameOptions.dxmtMetalSpatialUpscaleFactor.wrappedValue)))
-                                Slider(
-                                    value: $gameOptions.dxmtMetalSpatialUpscaleFactor,
-                                    in: 1.0...2.0,
-                                    step: 0.125
-                                )
-                                .help(localizedString(forKey: "metalFXSpatialHelp"))
-                            }
                         }
                     }
+                    HStack {
+                        Button("Save settings") {
+                            console.log("saving")
+                            persistUsrDefData(key: gameOptKey, data: GameOptionsData(data: gameOptions))
+                        }.buttonStyle(.borderedProminent)
+                        Button("Undo") {
+                            console.log("resetting")
+                            if let data: GameOptionsData = readUsrDefData(key: gameOptKey) {
+                                self.gameOptions.set(data: data)
+                            }
+                        }
+                        Button("Reset") {
+                            console.log("resetting")
+                            gameOptions.set(data: GameOptionsData(data: GameOptions()))
+                        }
+                    }.padding(.top)
                 }
-                HStack {
-                    Button("Save settings") {
-                        console.log("saving")
-                        persistUsrDefData(key: gameOptKey, data: GameOptionsData(data: gameOptions))
-                    }.buttonStyle(.borderedProminent)
-                    Button("Reset settings") {
-                        console.log("resetting")
-                        gameOptions.set(data: GameOptionsData(data: GameOptions()))
-                    }
-                }.padding(.top)
+                
             }
-            
+            .formStyle(.columns)
+            .toggleStyle(.switch)
+            //        .controlSize(/*@START_MENU_TOKEN@*/.mini/*@END_MENU_TOKEN@*/)
         }
-        .formStyle(.columns)
-        .toggleStyle(.switch)
-        //        .controlSize(/*@START_MENU_TOKEN@*/.mini/*@END_MENU_TOKEN@*/)
         .padding()
         .onAppear() {
             if let data: GameOptionsData = readUsrDefData(key: gameOptKey) {
